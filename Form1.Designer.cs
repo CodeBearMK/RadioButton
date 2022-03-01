1395e224c3830ef4ec415f48d40f48729a981034
cat32tw <mike1993xd@gmail.com>
2022-03-01 20:53:09 +0800

    RadioButton練習

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace RadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdb1 = new System.Windows.Forms.RadioButton();
+            this.rdb2 = new System.Windows.Forms.RadioButton();
+            this.rdb3 = new System.Windows.Forms.RadioButton();
+            this.rdb4 = new System.Windows.Forms.RadioButton();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.lblOp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rdb1
+            // 
+            this.rdb1.AutoSize = true;
+            this.rdb1.Checked = true;
+            this.rdb1.Location = new System.Drawing.Point(41, 46);
+            this.rdb1.Name = "rdb1";
+            this.rdb1.Size = new System.Drawing.Size(58, 19);
+            this.rdb1.TabIndex = 0;
+            this.rdb1.TabStop = true;
+            this.rdb1.Text = "加法";
+            this.rdb1.UseVisualStyleBackColor = true;
+            this.rdb1.CheckedChanged += new System.EventHandler(this.rdb1_CheckedChanged);
+            // 
+            // rdb2
+            // 
+            this.rdb2.AutoSize = true;
+            this.rdb2.Location = new System.Drawing.Point(144, 46);
+            this.rdb2.Name = "rdb2";
+            this.rdb2.Size = new System.Drawing.Size(58, 19);
+            this.rdb2.TabIndex = 0;
+            this.rdb2.Text = "減法";
+            this.rdb2.UseVisualStyleBackColor = true;
+            this.rdb2.CheckedChanged += new System.EventHandler(this.rdb2_CheckedChanged);
+            // 
+            // rdb3
+            // 
+            this.rdb3.AutoSize = true;
+            this.rdb3.Location = new System.Drawing.Point(241, 46);
+            this.rdb3.Name = "rdb3";
+            this.rdb3.Size = new System.Drawing.Size(58, 19);
+            this.rdb3.TabIndex = 0;
+            this.rdb3.Text = "乘法";
+            this.rdb3.UseVisualStyleBackColor = true;
+            this.rdb3.CheckedChanged += new System.EventHandler(this.rdb3_CheckedChanged);
+            // 
+            // rdb4
+            // 
+            this.rdb4.AutoSize = true;
+            this.rdb4.Location = new System.Drawing.Point(329, 46);
+            this.rdb4.Name = "rdb4";
+            this.rdb4.Size = new System.Drawing.Size(58, 19);
+            this.rdb4.TabIndex = 0;
+            this.rdb4.Text = "除法";
+            this.rdb4.UseVisualStyleBackColor = true;
+            this.rdb4.CheckedChanged += new System.EventHandler(this.rdb4_CheckedChanged);
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(41, 118);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 25);
+            this.txtN1.TabIndex = 1;
+            this.txtN1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(287, 118);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 25);
+            this.txtN2.TabIndex = 1;
+            this.txtN2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnEqual.Location = new System.Drawing.Point(41, 181);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(83, 41);
+            this.btnEqual.TabIndex = 2;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // lblAns
+            // 
+            this.lblAns.BackColor = System.Drawing.Color.DarkRed;
+            this.lblAns.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblAns.ForeColor = System.Drawing.Color.White;
+            this.lblAns.Location = new System.Drawing.Point(172, 170);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(215, 59);
+            this.lblAns.TabIndex = 3;
+            this.lblAns.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOp
+            // 
+            this.lblOp.AutoSize = true;
+            this.lblOp.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblOp.Location = new System.Drawing.Point(199, 120);
+            this.lblOp.Name = "lblOp";
+            this.lblOp.Size = new System.Drawing.Size(20, 20);
+            this.lblOp.TabIndex = 4;
+            this.lblOp.Text = "+";
+            this.lblOp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 269);
+            this.Controls.Add(this.lblOp);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.rdb4);
+            this.Controls.Add(this.rdb3);
+            this.Controls.Add(this.rdb2);
+            this.Controls.Add(this.rdb1);
+            this.Name = "Form1";
+            this.Text = "四則運算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdb1;
+        private System.Windows.Forms.RadioButton rdb2;
+        private System.Windows.Forms.RadioButton rdb3;
+        private System.Windows.Forms.RadioButton rdb4;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Label lblAns;
+        private System.Windows.Forms.Label lblOp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioButton
+{
+    public partial class Form1 : Form
+    {
+        int Res = 0, N1, N2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rdb1_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOp.Text = "+";
+            ClsTextBox();
+        }
+
+        private void rdb2_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOp.Text = "-";
+            ClsTextBox();
+        }
+
+        private void rdb3_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOp.Text = "*";
+            ClsTextBox();
+        }
+
+        private void rdb4_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOp.Text = "/";
+            ClsTextBox();
+        }
+
+        private void ClsTextBox()
+        {
+            txtN1.Text = "";
+            txtN2.Text = "";
+            lblAns.Text = "";
+        }
+
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                N1 = int.Parse(txtN1.Text);
+                N2 = int.Parse(txtN2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("請輸入數值！", "錯誤訊息", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (lblOp.Text == "+") Res = N1 + N2;
+            if (lblOp.Text == "-") Res = N1 - N2;
+            if (lblOp.Text == "*") Res = N1 * N2;
+            if (lblOp.Text == "/")
+            {
+                if (N2 == 0)
+                {
+                    MessageBox.Show("除數不能為零", "錯誤訊息", MessageBoxButtons.OK);
+                    return;
+                }
+                Res = N1 / N2;
+            }
+            lblAns.Text = Res.ToString();
+        }
+    }
+}
